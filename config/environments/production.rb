709abeb8b2f0f4d5a088622df7f1c40f666ad91d
icygirl276 <albany.alfonzo@hotmail.com>
2013-11-13 11:42:57 -0600

    implemented devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Required for Devise
+  # Note to set this to your actual host not localhost 3000
+  config.action_mailer.default_url_options = { :host => 'http://pbpinteresting.herokuapp.com/' }
 end
