785cabd55a1779a7c3ec10c5659a41af484c0a4f
icygirl276 <albany.alfonzo@hotmail.com>
2013-11-21 12:33:17 -0600

    Add amazon s3 for paperclip-heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,16 @@ Pinteresting::Application.configure do
   Paperclip.options[:command_path] = 'C:\ImageMagick-6.8.7-Q16'
 
 
+
+  #Sets paperclip to upload images to Amazon S3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
+
+
 end
